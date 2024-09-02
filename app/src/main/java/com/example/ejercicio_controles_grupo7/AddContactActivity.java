389daaad9d5dda196eed061fc4bdab1ce7faddd0
GJoseI <joseignaciogauna2@gmail.com>
2024-09-01 22:56:01 -0300

    Constraint layout y addcontact 2
    Cambien add contact 1 a constraint layout y hice la parte grafica de add contact 2

--- app/src/main/java/com/example/ejercicio_controles_grupo7/AddContactActivity.java
+++ app/src/main/java/com/example/ejercicio_controles_grupo7/AddContactActivity.java
@@ -22,6 +22,16 @@ public class AddContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_contact);
 
         //conexion con interfaz grafica
+        conexVariables();
+
+        // enlace de datos con spinner
+        String [] opciones={"casa","Trabajo","movil"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,opciones);
+        sp_tel.setAdapter(adapter);
+        sp_mail.setAdapter(adapter);
+    }
+
+    public void conexVariables (){
         sp_tel=(Spinner) findViewById(R.id.spinner_telefono);
         sp_mail=(Spinner) findViewById(R.id.spinner_email);
         et_nombre=(EditText) findViewById(R.id.txt_nombre);
@@ -30,13 +40,7 @@ public class AddContactActivity extends AppCompatActivity {
         et_mail=(EditText) findViewById(R.id.txt_email);
         et_telefono=(EditText) findViewById(R.id.txt_telefono);
         et_fecha_nacimiento=(EditText) findViewById(R.id.txt_fecha_nacimiento);
-        
 
-        // enlace de datos con spinner
-        String [] opciones={"casa","Trabajo","movil"};
-        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,opciones);
-        sp_tel.setAdapter(adapter);
-        sp_mail.setAdapter(adapter);
     }
 
     public void ContinuarButton(View view){
