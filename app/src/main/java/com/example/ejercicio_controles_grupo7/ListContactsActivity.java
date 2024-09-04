f19d8bb2d0adc757d5ce9373f39d6e0eb20def41
Mariano Benitez Grion <marianobenitezgrion@gmail.com>
2024-09-03 21:05:01 -0300

    Arreglo de error lectura json - listado de contactos funcional

--- app/src/main/java/com/example/ejercicio_controles_grupo7/ListContactsActivity.java
+++ app/src/main/java/com/example/ejercicio_controles_grupo7/ListContactsActivity.java
@@ -14,6 +14,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -28,35 +29,39 @@ public class ListContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_contacts);
-        lvContactos=(ListView) findViewById(R.id.lv_contactos);
+        lvContactos= findViewById(R.id.lv_contactos);
         try {
-            InputStream inputStream= getAssets().open("contacts.json");
+            FileInputStream inputStream= openFileInput("contacts.json");
             int size=inputStream.available();
             byte[] buffer= new byte[size];
-            inputStream.read(buffer);
+            int read;
+            read=inputStream.read(buffer);
+
+                if(read==size) {
+                    String json = new String(buffer, StandardCharsets.UTF_8);
+
+
+                    JSONArray jsonArray = new JSONArray(json);
+                    int max = jsonArray.length();
+                    ArrayAdapter<String> arrayContactos = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+
+
+                    for (int i = 0; i < max; i++) {
+                        Contacto contacto = new Contacto();
+                        JSONObject jsonObject =  jsonArray.getJSONObject(i);
+                        String nombre = jsonObject.getString("nombre");
+                        String apellido = jsonObject.getString("apellido");
+                        String email = jsonObject.getString("email");
+                        contacto.setNombre(nombre);
+                        contacto.setApellido(apellido);
+                        contacto.setEmail(email);
+                        arrayContactos.add(contacto.toStringResume());
+
+
+                    }
+                    lvContactos.setAdapter(arrayContactos);
+                }
             inputStream.close();
-            String json= new String(buffer, StandardCharsets.UTF_8);
-            JSONArray jsonArray= new JSONArray(json);
-            int max= jsonArray.length();
-            ArrayAdapter<String> arrayContactos= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
-            JSONObject jsonObject;
-
-            for (int i=0;i<max;i++){
-                Contacto contacto = new Contacto();
-                jsonObject=jsonArray.getJSONObject(i);
-                String nombre= jsonObject.getString("nombre");
-                String apellido= jsonObject.getString("apellido");
-                String email= jsonObject.getString("email");
-                contacto.setNombre(nombre);
-                contacto.setApellido(apellido);
-                contacto.setEmail(email);
-                arrayContactos.add(contacto.toStringResume());
-
-
-            }
-            lvContactos.setAdapter(arrayContactos);
-
-
 
         }catch (IOException e){
             Toast.makeText(this, "error leyendo archivo JSON", Toast.LENGTH_SHORT).show();
